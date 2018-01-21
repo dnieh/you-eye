08a8adb2c6d20d8e5512aa884b8453e924778179
Daniel Nieh <dnieh@zenreach.com>
2018-01-20 17:53:31 -0800

    Remove typography section.

--- src/index.js
+++ src/index.js
@@ -60,15 +60,6 @@ class SampleSite extends Component {
             onClick={this.onOpenModal.bind(this)}
           />
         </Screen>
-        <Screen>
-          <h2 className='logo'>typography</h2>
-          <h1>h1 H1 Heading</h1>
-          <h2>h2 H2 Heading</h2>
-          <h3>h3 H3 Heading</h3>
-          <p>
-            Paragraph: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-        </Screen>
       </div>
     )
   }
